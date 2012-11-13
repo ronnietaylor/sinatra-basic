f2d007ab272b53e9cf2b75004d33d2b0fa95ed7c
Alan <alan@ronnietaylor.com>
2012-11-13 11:50:09 -0500

    fixing checkbox validation

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
   console.log('app.js file example');
-  $('#validation-form').validate();
+  $('#validation-form').validate( {
+      rules: {
+          services: {
+              required: true,
+              minlength: 1
+          }
+       }
+   });
+   $('#validation-form').submit(function(e){
+   	
+   	$("input[name='services']:checked").each(function(){
+   		console.log($(this).attr("value"));
+   	});
+		return false;
+   });
 });
